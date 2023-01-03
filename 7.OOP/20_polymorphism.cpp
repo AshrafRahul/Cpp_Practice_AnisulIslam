8940329042c48e13332d1a06b1c0ac89d5732c98
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-01-03 13:47:58 +0600

    OOP || Polymorphism

---
+++ 7.OOP/20_polymorphism.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Shape{
+    public:
+        double dim1, dim2;
+
+        Shape(double dim1, double dim2){
+            this -> dim1 = dim1;
+            this -> dim2 = dim2;
+        }
+        virtual double area(){
+            return 0;
+        }
+};
+
+class Triangle : public Shape{
+    public:
+        Triangle(double dim1, double dim2) : Shape(dim1, dim2){
+            
+        }
+
+        double area(){
+            return 0.5*dim1*dim2;
+        }
+};
+
+class Rectangle : public Shape{
+    public:
+        Rectangle(double dim1, double dim2) : Shape(dim1, dim2){
+            
+        }
+
+        double area(){
+            return dim1*dim2;
+        }
+};
+
+int main(){
+    Shape *p;
+    Triangle t(10,20);
+    Rectangle r(10,20);
+
+    p = &t;
+    cout<<"Are of Triangle: "<<p -> area()<<endl;
+
+    p = &r;
+    cout<<"Are of Rectangle: "<<p -> area()<<endl;
+
+    return 0;
+}
